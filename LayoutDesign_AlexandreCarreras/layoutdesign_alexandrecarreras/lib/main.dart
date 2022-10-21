cd29d874ba827bd29db487ea5d8410c167a8c6ff
TheGewehr <alexandre07979@gmail.com>
2022-10-21 14:27:58 +0200

    Adding some widgets more

--- LayoutDesign_AlexandreCarreras/layoutdesign_alexandrecarreras/lib/main.dart
+++ LayoutDesign_AlexandreCarreras/layoutdesign_alexandrecarreras/lib/main.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 5, 5, 5),
         appBar: AppBar                                            // Appbar Stuff
         (
-          backgroundColor: Colors.grey,
+          backgroundColor: Color.fromARGB(255, 49, 49, 49),
           actions: <Widget> 
           [
             IconButton(
@@ -38,7 +40,15 @@ class MyApp extends StatelessWidget {
             icon: const Icon(Icons.west,color: Colors.purple,)
           ),
         ),
-        body: Align(alignment: Alignment.centerRight, child: LikeWidget())//Center(child: LikeWidget()
+        bottomNavigationBar: BottomBar(),
+        body:  Stack(
+          children: [
+          Align(alignment: Alignment.centerRight, child: LikeWidget(),),
+          Align(alignment: Alignment.topRight, child: CountOfPictures(),),
+          //Align(alignment: Alignment.bottomCenter, child: AddToTheCart(),),
+          
+          ]
+        )
       )
     );
     
@@ -50,7 +60,6 @@ class LikeWidget extends StatelessWidget{
   bool isLiked = false;
   LikeWidget({Key? key}) : super(key: key);
 
-  static const double width = 60;
   static const double padding = 4;
 
   @override
@@ -71,6 +80,80 @@ class LikeWidget extends StatelessWidget{
           icon: const Icon(Icons.favorite_border,color: Color.fromARGB(255, 71, 71, 71), size: 31,)
         ),
     );
+  }
+}
+
+class CountOfPictures extends StatelessWidget{
+  
+  const CountOfPictures({Key? key}) : super(key: key);
+
+  
+  static const double padding = 6;
+
+  @override
+  Widget build(BuildContext)
+  {
+    return Container(
+      width: 70,
+      height: 40, 
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 202, 202, 202),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        
+        ),
+      padding: const EdgeInsets.all(padding),
+      child: Center(child: Text("1 of 11", style: const TextStyle( fontSize: 18, fontWeight: FontWeight.bold,color: Colors.purple)))
+    );
+    
+  }
+}
+
+class BottomBar extends StatelessWidget{
+  
+ const BottomBar({Key? key}) : super(key: key);  
+
+  @override
+  Widget build(BuildContext)
+  {
+    return  Container( 
+               
+      child: const BottomAppBar(
+        elevation: 0,
+        color: Color.fromARGB(255, 49, 49, 49),
+        child: SizedBox(
+          height: 52,
+          //width: MediaQuery.of(context).size.width,
+          child: Center(child: AddToTheCart())
+        )
+        
+      ,)
+      
+    );
+    
+  }
+}
+
+class AddToTheCart extends StatelessWidget{
+  const AddToTheCart({Key? key}) : super(key: key);
+
+  
+  static const double padding = 4;
+
+  @override
+  Widget build(BuildContext)
+  {
+    return Container(
+      
+      height: 40, 
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 201, 72, 184),
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        
+        ),
+      padding: const EdgeInsets.all(padding),
+      child: Center(child: Text("Add to the Cart", style: const TextStyle( fontSize: 18, fontWeight: FontWeight.bold,color: Color.fromARGB(255, 255, 255, 255))))
+    );
+    
   }
 }
 
